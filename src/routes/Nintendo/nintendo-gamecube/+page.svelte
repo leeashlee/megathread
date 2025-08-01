0df893febff7a307a165806c91d94a6de6e0b5a1
Noel Tugy <78592522+leeashlee@users.noreply.github.com>
2025-08-01 06:19:49 +0300

    update 2 files and move 1 file

--- src/routes/Nintendo/nintendo-gamecube/+page.svelte
+++ src/routes/Nintendo/nintendo-gamecube/+page.svelte
@@ -4,8 +4,17 @@
 <!-- Home page for the ROMs megathread -->
 <div class="mx-8 mb-8 mt-8 space-y-4 bg-background">
     <h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">
-        Metroid
+        Nintendo GameCube
     </h1>
+    <p class="leading-7 [&:not(:first-child)]:mt-6">
+        RVZ format is recommended for its size and convenience. For more
+        information, please refer to the
+        <a
+            href="/Populars/about-rvz-files"
+            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+            >RVZ format page</a
+        >.
+    </p>
     <div class="my-6 w-full overflow-y-auto">
         <table class="w-full">
             <thead>
@@ -20,6 +29,11 @@
                     >
                         Myrient (Fast & Unrestricted)
                     </th>
+                    <th
+                        class="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Internet Archive
+                    </th>
                 </tr>
             </thead>
             <tbody>
@@ -27,13 +41,37 @@
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
-                        Metroid Fusion (GBA) (USA)
+                        Nintendo - GameCube - NKit RVZ [zstd-19-128k]
                     </td>
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
                         <a
-                            href="https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/Metroid%20Fusion%20(USA).zip"
+                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                    </td>
+                </tr>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        RVZ - Nintendo GameCube (Redump) - USA Set
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://archive.org/download/rvz-gc-usa-redump/RVZ-GC-USA-REDUMP/"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
@@ -43,13 +81,17 @@
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
-                        Metroid Zero Mission (GBA) (USA)
+                        RVZ - Nintendo GameCube (Redump) - Europe Set
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
                     </td>
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
                         <a
-                            href="https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/Metroid%20-%20Zero%20Mission%20(USA).zip"
+                            href="https://archive.org/download/rvz-gc-europe-redump/RVZ-GC-EUROPE-REDUMP/"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
@@ -59,13 +101,17 @@
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
-                        Metroid Prime Trilogy (Wii) (USA)
+                        RVZ - Nintendo GameCube (Redump) - Asia Set
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
                     </td>
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
                         <a
-                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%20Trilogy%20(USA).zip"
+                            href="https://archive.org/download/rvz-gc-asia-redump/RVZ-GC-ASIA-REDUMP/"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
@@ -75,13 +121,52 @@
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
-                        Metroid Prime (GC) (USA)
+                        Redump - Nintendo - GameCube (America) (20160527)
                     </td>
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
                         <a
-                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%20(USA).zip"
+                            href="https://myrient.erista.me/files/Internet%20Archive/Unknown/RedumpNintendoGameCubeAmerica/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Part 1</a
+                        > /
+                        <a
+                            href="https://myrient.erista.me/files/Internet%20Archive/Unknown/RedumpNintendoGameCubeAmericaPart2/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Part 2</a
+                        > /
+                        <a
+                            href="https://myrient.erista.me/files/Internet%20Archive/Unknown/RedumpNintendoGameCubeAmericaPart3/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Part 3</a
+                        >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                    </td>
+                </tr>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Redump - Nintendo - GameCube (Japan)
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://myrient.erista.me/files/Internet%20Archive/pixelspoil/NCubeJ/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://archive.org/download/NCubeJ"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
@@ -91,29 +176,62 @@
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
-                        Metroid Prime 2 (GC) (USA)
+                        Redump - Nintendo - GameCube (Europe)
                     </td>
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
                         <a
-                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%202%20-%20Echoes%20(USA).zip"
+                            href="https://myrient.erista.me/files/Internet%20Archive/kodi_amp_spmc_canada/EuropeanGamecubeCollectionByGhostware/"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
                     </td>
                 </tr>
                 <tr class="even:bg-muted m-0 border-t p-0">
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
-                        Metroid Prime 3 (Wii) (USA)
+                        Redump - Nintendo - GameCube (Australia)
                     </td>
                     <td
                         class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
                     >
                         <a
-                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%203%20-%20Corruption%20(USA).zip"
+                            href="https://myrient.erista.me/files/Internet%20Archive/kodi_amp_spmc_canada/AustraliaGamecubeCollectionByGhostware/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                    </td>
+                </tr>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Redump - Nintendo - GameCube (Asia)
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://myrient.erista.me/files/Internet%20Archive/kodi_amp_spmc_canada/AsiaGamecubeCollectionByGhostware/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://archive.org/download/AsiaGamecubeCollectionByGhostware"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
--- src/routes/Nintendo/nintendo-games/+page.svelte
+++ src/routes/Nintendo/nintendo-games/+page.svelte
@@ -4,40 +4,77 @@
 <!-- Home page for the ROMs megathread -->
 <div class="mx-8 mb-8 mt-8 space-y-4 bg-background">
     <h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">
-        About RVZ files
+        Nintendo Games
     </h1>
-    <p
-        class="text-muted-foreground text-xl leading-7 [&:not(:first-child)]:mt-6"
-    >
-        RVZ files work with the Dolphin emulator but not on real hardware. They
-        also help save bandwidth for both you and the website hosting the files
-        when downloading. If you are using real hardware, use <a
-            href="https://dolphin-emu.org/"
-            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
-            >Dolphin Emulator</a
-        >
-        to convert the files to ISO<br />
-        <img
-            src="/src/img/9K4CIDd.png"
-            alt=""
-            class="h-full w-full rounded-md object-cover"
-        />
-        <img
-            src="/src/img/75nhplt.png"
-            alt=""
-            class="h-full w-full rounded-md object-cover"
-        />
+    <p class="leading-7 [&:not(:first-child)]:mt-6">
+        If you are looking for Retro ROMs, see the <a href="#retro-games">Retro Games page</a>.
     </p>
-    <!-- svelte-ignore a11y_media_has_caption -->
-    <video controls class="h-full w-full rounded-md object-cover"
-        autoplay
-        muted
-        playsinline
-        preload="metadata"
-        style="aspect-ratio: 16 / 9"
-        ><source
-            src="https://myrient.erista.me/assets/rvz.mp4"
-            type="video/mp4"
-        /></video
-    >
+    <div class="my-6 w-full overflow-y-auto">
+        <table class="w-full">
+            <thead>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <th
+                        class="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Title
+                    </th>
+                    <th
+                        class="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Myrient (Fast & Unrestricted)
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Fire Emblem Path of Radiance (GC) (USA)
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/Fire%20Emblem%20-%20Path%20of%20Radiance%20(USA).zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                </tr>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Fire Emblem Radiant Dawn (Wii) (USA)
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/Fire%20Emblem%20-%20Radiant%20Dawn%20(USA)%20(Rev%201).zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                </tr>
+                <tr class="even:bg-muted m-0 border-t p-0">
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        Fire Emblem Fates Special Edition (3DS) (USA)
+                    </td>
+                    <td
+                        class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"
+                    >
+                        <a
+                            href="https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Encrypted)/Fire%20Emblem%20Fates%20-%20Special%20Edition%20(USA).zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
