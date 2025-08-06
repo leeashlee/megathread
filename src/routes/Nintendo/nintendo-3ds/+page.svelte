0649d8e929393eb194aad146acc29fdc2aa90584
Noel Tugy <78592522+leeashlee@users.noreply.github.com>
2025-08-06 10:55:54 +0300

    update +page.svelte

--- src/routes/Nintendo/nintendo-3ds/+page.svelte
+++ src/routes/Nintendo/nintendo-3ds/+page.svelte
@@ -221,7 +221,7 @@
             3DS/CIA Decryption InsTableRowuctions
         </h2>
         <p class="leading-7 [&:not(:first-child)]:mt-6">
-            If CiTableRowa says the ROM is encrypted, use <a
+            If Citra says the ROM is encrypted, use <a
                 href="https://gbatemp.net/download/batch-cia-3ds-decryptor.35098/"
                 class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                 >Batch CIA-3DS Decryptor</a
@@ -234,7 +234,7 @@
                 class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                 >this script</a
             >.<br />
-            Or, add the following AES keys to CiTableRowa from
+            Or, add the following AES keys to Citra from
             <a
                 href="https://pastebin.com/vRy8c6JP"
                 class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
@@ -258,7 +258,7 @@
             Add the downloaded Pastebin TXT file to <code
                 class="bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
             >
-                C:/Users/(Your Username)/AppData/Roaming/CiTableRowa/sysdata
+                C:/Users/(Your Username)/AppData/Roaming/Citra/sysdata
             </code>
             and make sure it is named
             <code
@@ -277,7 +277,7 @@
             <code
                 class="bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
             >
-                ~/.local/share/ciTableRowa-emu/sysdata
+                ~/.local/share/Citra-emu/sysdata
             </code>
             and make sure it is named
             <code
@@ -286,9 +286,9 @@
                 aes_keys.txt
             </code>. For more information, refer to the
             <a
-                href="https://web.archive.org/web/20240304205930/https://ciTableRowa-emu.org/wiki/user-directory/"
+                href="https://web.archive.org/web/20240304205930/https://Citra-emu.org/wiki/user-directory/"
                 class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
-                >CiTableRowa Wiki</a
+                >Citra Wiki</a
             >.
         </p>
     </div>
